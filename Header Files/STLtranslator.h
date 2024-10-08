5d7e96897404f3bade6292589b1dc2a992514b9b
CCTech-Prerana-Jadhav <prerana.jadhav@cctech.co.in>
2024-10-08 12:45:52 +0530

    first commit

---
+++ Header Files/STLtranslator.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Triangulations.h"
+#include <vector>
+#include <string>
+
+class FileHandler {
+private:
+    std::string inputFile;
+
+public:
+    FileHandler(const std::string& input);
+    void readSTLFile(Triangulation& triangulation);
+    void writeVerticesToFile(const std::vector<Point>& vertices);
+
+private:
+    void parseSTLLine(const std::string& line, Triangulation& triangulation, std::vector<Point>& vertices);
+};
