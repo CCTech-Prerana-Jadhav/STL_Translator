536ca050260937a22f25d7b774ee30da065f7c06
CCTech-Prerana-Jadhav <prerana.jadhav@cctech.co.in>
2024-10-08 15:05:08 +0530

    Update Point.h

--- Header Files/Point.h
+++ Header Files/Point.h
@@ -1,9 +1,15 @@
 #pragma once
 
+using namespace std;
+
 class Point {
 public:
-    int x, y, z;
-
     Point(int x = 0, int y = 0, int z = 0);
-};
+    int getX() const;
+    int getY() const;
+    int getZ() const;
+    ~Point();
 
+private:
+    int x, y, z;
+};
