682fa991afbe38e24d17a24660c2e595772b49cf
CCTech-Prerana-Jadhav <prerana.jadhav@cctech.co.in>
2024-10-16 16:31:59 +0530

    Add files via upload

---
+++ stlToDatTranslator/Source Files/3D_ShapePlotter.cpp
@@ -0,0 +1,7 @@
+#include "Driver.h"
+
+int main() {
+    ShapePlotter plotter;
+    plotter.convertSTLToDAT("C:\\Users\\Prerna Jadhav\\source\\repos\\ShapePlotter\\3D_ShapePlotter\\Test Cases\\Sphere.stl", "C:\\Users\\Prerna Jadhav\\source\\repos\\ShapePlotter\\3D_ShapePlotter\\Test Cases\\data.dat");  
+    return 0;
+}
