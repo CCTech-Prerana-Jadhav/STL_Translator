639978933d43daab86815ff705197c9a4f0c83a8
CCTech-Prerana-Jadhav <prerana.jadhav@cctech.co.in>
2024-10-16 16:29:24 +0530

    Delete Header Files directory

--- Header Files/Writer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <fstream>
-#include "Triangle.h"
-#include <vector>
-
-class Writer {
-public:
-    void writeToDAT(const std::string& outputFile, const std::vector<Triangle>& triangles, const std::vector<double>& uniqueCoordinates);
-};
